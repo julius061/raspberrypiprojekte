6a31815681ecb1f92be75a1f1eee1823aab78760
julius061 <julius.steinhaeuser@mesalogic.de>
2023-08-17 14:41:10 +0200

    yee

--- webserver/scripts/PiControl.js
+++ webserver/scripts/PiControl.js
@@ -3,13 +3,13 @@ document.getElementById("onButton").addEventListener("click", ledOn, false);;
 document.getElementById("offButton").addEventListener("click", ledOff, false);
 
 // Turn LED on
-function ledOn() {
-    document.getElementById("onButtonBool").value = true;
-    location.reload();
+function ledOn() {/*
+    console.log("test");
+    window.open("https://127.0.0.1:8080/index.html?isButtonOnPressed=true&isButtonOffPressed=false");
+    window.open("https://google.de/");*/
 } 
 
 // Turn LED off
-function ledOff() {
-    document.getElementById("offButtonBool").value = true;
-    location.reload()
+function ledOff() {/*
+    window.open("https://127.0.0.1:8080/index.html?isButtonOnPressed=false&isButtonOnPressed=false");*/
 }
